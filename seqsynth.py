7e417fa1f4bca32f54ae6fe9ba478936f5116cbd
Kristján Eldjárn <kristjan@eldjarn.net>
2022-03-01 13:18:37 -0800

    Added method to parse event spec

--- seqsynth.py
+++ seqsynth.py
@@ -21,7 +21,26 @@ class SeqSynth:
 
         # Setup
         self.parse_gtf()
-        print(self.txs)
+        self.scaffolds = parse_fasta(self.fa)
+
+
+        '''
+        for k, tx in self.txs.items():
+            print(k)
+            for utr in tx['utrs']:
+                print(utr['feature'], utr['strand'])
+                utr = self.scaffolds[utr['scaffold']][utr['start']-10:utr['end']+10]
+                if 'AATAAA' in utr:
+                    print('Found a thing!')
+                    print(utr)
+                    print(utr.count('AATAAA'))
+                if 'TTTATT' in utr:
+                    print('Found a reciprocal thing!')
+                    print(utr)
+                    print(utr.count('TTTATT'))
+
+            print('==========')
+        '''
 
     """
       ____________
@@ -59,7 +78,7 @@ class SeqSynth:
                     'scaffold': data[0],
                     'feature': data[2].lower(),
                     'start': int(data[3]) - 1,
-                    'end': int(data[4]) - 1,
+                    'end': int(data[4]),
                     'strand': data[6],
                 }
                 fields['rest'] = parse_rest(data[8], '.gtf')
@@ -91,21 +110,28 @@ class SeqSynth:
                     lb = min(fields['start'], lb)
                     ub = max(fields['end'],   ub)
 
+        for tx in self.txs.values():
+            tx['exons'].sort(key=lambda x: x['rest']['exon_number'])
+
+        # Create introns (maybe move to 'generate_retention')
         for k, tx in self.txs.items():
             ivs = [(f['start'], f['end']) for f in tx['exons']]
             tx['introns'] = get_complement(ivs, lb, ub)
 
-        for tx in self.txs.values():
-            tx['exons'].sort(key=lambda x: x['rest']['exon_number'])
-
         print(f'Found {len(tx.keys())} transcripts belonging to {Fore.RED}{",".join(self.gene)}{Style.RESET_ALL}')
 
-    def parse_event_spec(self, event_spec):
-        this.event_spec = [
-            {'type': 'duplication', 'pheno': 0.2},
-            {'type': 'skip', 'pheno': -0.2},
-            {'type': 'APA', 'pheno': 0.5}
-        ]
+    def parse_event_spec(self, events):
+        types = {'D': 'duplication', 'S': 'skip', 'R': 'retention', 'A': 'APA'}
+        for e in events:
+            try:
+                t = types[e[0].upper()]
+                p = float(e[1:])
+            except KeyError as err:
+                raise KeyError(f'Cannot parse event type {e[0]}')
+            except ValueError as err:
+                raise ValueError(f'Cannot parse effect size {e[1:]}')
+
+            self.event_spec.append({'type': t, 'pheno': p})
 
     """
       ________________________
@@ -115,7 +141,7 @@ class SeqSynth:
     |/_______________________/
 
     """
-    def generate_reads(self):
+    def generate_rna(self):
         pass
 
     def generate_duplication(self, tx, dups):
@@ -131,13 +157,46 @@ class SeqSynth:
         self.txs[tx_id] = new_tx
 
     def generate_exon_skip(self, tx, skips):
-        pass
+        new_tx = copy.deepcopy(self.txs[tx])
+        exons = new_tx['exons']
+        i, j = random.choices(range(len(exons)), k=2)
+        if j < i:
+            i, j = j, i
+        exons = exons[:i] + exons[j+1]
+        new_tx['exons'] = exons
+        tx_id = f"{new_tx['name']}_S{skips}"
+        new_tx['name'] = tx_id
+        self.txs[tx_id] = new_tx
 
     def generate_APA(self, tx, skips):
+        # TODO:
+        # Figure out APA events
+
+        tx['fwd_apa_motifs'] = sum(x.count('AATAAA') for x in tx['utrs'])
+        tx['rv_apa_motifs'] = sum(x.count('TTTATT') for x in tx['utrs'])
         pass
 
-    def generate_retention(self, tx, skips):
-        pass
+    def generate_retention(self, tx, rets):
+        new_tx = copy.deepcopy(self.txs[tx])
+        exons = new_tx['exons']
+        i = random.choice(range(len(exons)))
+
+        if exons[i]['strand'] == '-':
+            pred = lambda x: x[1] <= exons[i]['start']
+        else:
+            pred = lambda x: x[0] >= exons[i]['end']
+
+        intron = next(x for x in new_tx['introns'] if pred(x))
+        intron = {
+            'start' : intron[0],
+            'end'   : intron[1],
+            'strand': exons[i]['strand']
+        }
+
+        new_tx['exons'] = exons[:i+1] + [intron] + exons[i+1:]
+        tx_id = f"{new_tx['name']}_S{rets}"
+        new_tx['name'] = tx_id
+        self.txs[tx_id] = new_tx
 
     def generate_events(self):
         phenotypes = {}
@@ -214,20 +273,31 @@ class SeqSynth:
                     fh.write(f'{sj}\n')
         print(f'{wrong_scaffolds} scaffolds not found')
 
+    def render_reads(self):
+        pass
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Generate simulated RNA reads for a cohort.'
     )
-    parser.add_argument('gtf', help='Transcriptome annotation', type=str)
-    parser.add_argument('fa', help='Genome scaffolds', type=str)
+    parser.add_argument('--gtf', help='Transcriptome annotation', type=str)
+    parser.add_argument('--fa', help='Genome scaffolds', type=str)
+    parser.add_argument('--N', help='Cohort size', type=int)
+    parser.add_argument('--depth', help='Mean read depth', type=int)
     parser.add_argument('--genes', help='The gene(s) to generate reads for',
                         nargs='*', type=str)
+    parser.add_argument('--events', help='''
+            Denote events and phenotypes. D0.1 denotes a duplication event with
+            a phenotype 10 percent correlated with it. Other options include A:
+            alternative poly-A, S: exon skip, R: intron retention.
+            ''', nargs='*', type=str)
     parser.add_argument('--out', help='The output directory', nargs=1, type=str)
-    # TODO:
-    #       Add args.event_spec
+
     args = parser.parse_args()
 
     seq = SeqSynth(args.gtf, args.fa, args.genes)
     #seq.parse_event_spec(args.event_spec)
+
+    seq.generate_reads(args.N, args.depth)
 
 
